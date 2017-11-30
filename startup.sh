22d48b152f000d645b2f82e66f797af17c3c6acb
Matt Todd <matt.todd@resonate.tech>
2017-11-30 22:03:14 +0000

    Another typo

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@ docker config create metricbeat.yml metricbeat/metricbeat.yml
 docker config create logstash-template logstash/logstash-template.json 
 docker config create logstash-conf logstash/logstash.conf
 
-docker stack deploy -c docker-compose.yml "elasticserch$1"
+docker stack deploy -c docker-compose.yml "elasticsearch$1"
 
