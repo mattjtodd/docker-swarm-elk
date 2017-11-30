fa645cad7b87e5fe3fdd31c2a0c49c6dd1775276
Matt Todd <matt.todd@resonate.tech>
2017-11-30 18:10:06 +0000

    Added dashboards

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker config create metricbeat.yml metricbeat/metricbeat.yml
+docker config create logstash-template logstash/logstash-template.json 
+docker config create logstash-conf logstash/logstash.conf
 
 docker stack deploy -c docker-compose.yml "elasticserch$1"
 
