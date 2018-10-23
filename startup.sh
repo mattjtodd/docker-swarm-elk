97e0f8de01d1133ee0c34ae060fd9a4eff805c47
Matt Todd <matt.todd@resonate.tech>
2018-10-23 14:58:33 +0100

    Added advisor

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-docker config create metricbeat.yml metricbeat/metricbeat.yml
+docker config create metricbeat-conf metricbeat/metricbeat.yml
 docker config create logstash-template logstash/logstash-template.json 
 docker config create logstash-conf logstash/logstash.conf
+docker config create nginx-conf nginx/nginx.conf
 
 docker stack deploy -c docker-compose.yml "elasticsearch$1"
 
